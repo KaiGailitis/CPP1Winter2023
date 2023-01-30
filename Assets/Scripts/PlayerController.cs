8c66708fbb9c8c8ef9cc976029a83fe989bdc5f6
Kai <119766049+KaiGailitis@users.noreply.github.com>
2023-01-29 22:12:42 -0500

    Lab 3 - Completed

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer sr;
+
+    // Movement
+    public float speed;
+    public float jumpForce;
+
+    // GroundCheck
+    public Transform groundCheck;
+    public LayerMask isGroundLayer;
+    public bool isGrounded;
+    public float groundCheckRadius;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 400.0f;
+        }
+
+        if(!groundCheck)
+        {
+            groundCheck = GameObject.FindGameObjectWithTag("GroundCheck").transform;
+        }
+
+        if(groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.2f;
+        }
+    }
+
+    void Update()
+    {
+
+        AnimatorClipInfo[] curPlayingClip = anim.GetCurrentAnimatorClipInfo(0);
+        float hInput = Input.GetAxis("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer); 
+        
+        if (curPlayingClip.Length > 0)
+        {
+            if (Input.GetButtonDown("Fire1") && curPlayingClip[0].clip.name != "Fire")
+                anim.SetTrigger("Fire");
+            else if (curPlayingClip[0].clip.name == "Fire")
+                rb.velocity = Vector2.zero;
+            else
+            {
+                Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+                rb.velocity = moveDirection;
+            }
+        }
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        if (!isGrounded && Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("JumpAttack");
+        }
+
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+        if  (hInput != 0)
+        {
+            sr.flipX = (hInput < 0);
+        }
+    }
+}
